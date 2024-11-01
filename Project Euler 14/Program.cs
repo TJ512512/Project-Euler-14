488eabaebbf83696728f495f78826cb8b86d30f6
TJ512512 <160138699+TJ512512@users.noreply.github.com>
2024-11-01 11:20:50 +0000

    Add files via upload

---
+++ Project Euler 14/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Project_Euler_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger BiggestChain = 0;
+            //Console.WriteLine(Numbers[0]); 
+            for (BigInteger i = 1; i <= 1000000; i++)
+            {
+                BigInteger chain = CalculateingTheLength(i);
+                if (chain > BiggestChain)
+                {
+                    BiggestChain = chain;
+                    Console.WriteLine(i.ToString());
+                }
+            }
+        }
+        static BigInteger CalculateingTheLength(BigInteger number)
+        {
+            //Console.WriteLine(number);
+            BigInteger counter = 0;
+            while (number != 1)
+            {
+                //Console.WriteLine(number);
+                counter++;
+                if (number % 2 == 0)
+                {
+                    number /= 2;
+                }
+                else
+                {
+                    number *= 3;
+                    number++;
+                }
+            }
+            return counter;
+        }
+    }
+}
